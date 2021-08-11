e3a1cda5e3c240d8226781ea621e42504ffa2d08
Matthew <mblair1@citadel.edu>
2021-08-11 11:51:21 -0400

    First post

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Matthew Blair',
+        'title': 'Feed Post 1',
+        'content': 'First post content',
+        'date_posted': 'Auguest 11, 2021'
+    },
+    {
+        'author': 'Kenndog',
+        'title': 'Beethoven',
+        'content': 'Homies with the guap',
+        'date_posted': 'Auguest 11, 2021'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
